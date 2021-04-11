397b20de7fa8e3563dcc0ca8a37eb0db369b855b
andrejtachko <atacigin@gmail.com>
2021-04-11 22:25:54 +0200

    add a bit style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,11 +10,11 @@ export default () => {
       <main className="max-w-4xl flex-grow mx-auto flex flex-col justify-around">
         <div className="sm:flex sm:flex-row-reverse sm:items-center">
           <div className="sm:px-2">
-            <h1 className="px-4 pt-5 text-2xl text-left text-teal-500 font-bold sm:text-3xl">
-              Andrej Tacigin
+            <h1 className="px-4 pt-2 text-2xl text-left text-teal-200 font-bold sm:text-3xl">
+              Andrej Tačigin
             </h1>
-            <p className="px-4 mt-8 text-lg text-teal-500 sm:mt-8">
-              Born in Zagreb, based in Berlin.
+            <p className="px-4 pt-4 mt-8 text-lg text-teal-500 sm:mt-8">
+              Frontend developer, born in Zagreb, based in Berlin.
             </p>
             <p className="px-4 mt-8 text-lg text-teal-500 sm:mt-8">
               Professional experience with React with Typescript, Redux, Redux
@@ -30,10 +30,10 @@ export default () => {
         </div>
         <div className="flex">
           <a href="https://www.linkedin.com" target="blank">
-            <img src={yo} />
+            <img src={yo} className="h-20 w-20" alt="Linkedin" />
           </a>
           <a href="https://www.github.com" target="blank">
-            <img src={github} alt="" />
+            <img src={github} className="h-20 w-20" alt="Github" />
           </a>
         </div>
 
