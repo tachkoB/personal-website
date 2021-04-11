c12832eeb1fbf18129791fa0d1546deabeed7668
andrejtachko <atacigin@gmail.com>
2021-04-11 22:48:08 +0200

    stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,13 +7,13 @@ export default () => {
 
   return (
     <Layout>
-      <main className="max-w-4xl flex-grow mx-auto flex flex-col justify-around">
+      <main className="max-w-4xl flex-grow mx-auto flex flex-col justify-around p-8">
         <div className="sm:flex sm:flex-row-reverse sm:items-center">
           <div className="sm:px-2">
             <h1 className="px-4 pt-2 text-2xl text-left text-teal-200 font-bold sm:text-3xl">
               Andrej Tačigin
             </h1>
-            <p className="px-4 pt-4 mt-8 text-lg text-teal-500 sm:mt-8">
+            <p className="px-4 pt-10 mt-8 text-lg text-teal-500 sm:mt-8">
               Frontend developer, born in Zagreb, based in Berlin.
             </p>
             <p className="px-4 mt-8 text-lg text-teal-500 sm:mt-8">
@@ -29,16 +29,23 @@ export default () => {
           </div>
         </div>
         <div className="flex">
-          <a href="https://www.linkedin.com" target="blank">
-            <img src={yo} className="h-20 w-20" alt="Linkedin" />
-          </a>
-          <a href="https://www.github.com" target="blank">
-            <img src={github} className="h-20 w-20" alt="Github" />
-          </a>
+          <div className="w-6/12 content-center">
+            <a
+              href="https://www.linkedin.com/in/andrej-tacigin-tachko/"
+              target="blank"
+            >
+              <img src={yo} className="h-20 w-20 mx-auto" alt="Linkedin" />
+            </a>
+          </div>
+          <div className="w-6/12">
+            <a href="https://github.com/tachkoB" target="blank">
+              <img src={github} className="h-20 w-20 mx-auto" alt="Github" />
+            </a>
+          </div>
         </div>
 
-        <div className="flex items-center p-3 mx-2 bg-white rounded shadow-xs sm:mx-auto">
-          Get in touch
+        <div className="flex items-center mx-2 text-teal-500 shadow-xs sm:mx-auto">
+          Get in touch through atacigin@gmail.com
         </div>
       </main>
     </Layout>
