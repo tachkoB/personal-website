e76f2d0fe0122035bd7cbf5cfec435b7ee6e386a
andrejtachko <atacigin@gmail.com>
2021-04-12 00:21:55 +0200

    create colors for theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,20 +40,20 @@ export default () => {
         <main className="max-w-4xl flex-grow mx-auto flex flex-col justify-around p-8">
           <div className="sm:flex sm:flex-row-reverse sm:items-center">
             <div className="sm:px-2">
-              <h1 className="px-4 pt-2 text-2xl text-left text-teal-200 font-bold sm:text-3xl">
+              <h1 className="px-4 pt-2 text-2xl text-left text-primary  font-bold sm:text-3xl">
                 Andrej Tačigin
               </h1>
-              <p className="px-4 pt-10 mt-8 text-lg text-teal-500 sm:mt-8">
+              <p className="px-4 pt-10 mt-8 text-lg text-secondary sm:mt-8">
                 Frontend developer, born in Zagreb, based in Berlin.
               </p>
-              <p className="px-4 mt-8 text-lg text-teal-500 sm:mt-8">
+              <p className="px-4 mt-8 text-lg text-secondary sm:mt-8">
                 Professional experience with React with Typescript, Redux, Redux
                 Saga, Material UI, Styled components..
               </p>
-              <p className="px-4 mt-8 text-lg text-teal-500  sm:mt-8">
+              <p className="px-4 mt-8 text-lg text-secondary  sm:mt-8">
                 Big fan of React Testing library and Cypress.
               </p>
-              <p className="px-4 mt-8 text-lg  text-teal-500  sm:mt-8">
+              <p className="px-4 mt-8 text-lg  text-secondary  sm:mt-8">
                 Enjoy clean, well documented code.
               </p>
             </div>
@@ -74,7 +74,7 @@ export default () => {
             </div>
           </div>
 
-          <div className="flex items-center mx-2 text-teal-500 shadow-xs sm:mx-auto">
+          <div className="flex items-center mx-2 text-secondary shadow-xs sm:mx-auto">
             <p>
               Get in touch through{"  "}
               <span onClick={handleClick} className="cursor-pointer">
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@ module.exports = {
   purge: [],
   target: "relaxed",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#9CFF00',
+        secondary: '#0C8900',
+      }
+    },
   },
   variants: {},
   plugins: [],
